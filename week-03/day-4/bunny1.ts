dea9515c162129be57582cd8905d1b783683df8c
Daniel Kasza <daniel.kasza93@gmail.com>
2018-08-16 14:16:42 +0200

    power

---
+++ week-03/day-4/bunny1.ts
@@ -0,0 +1,12 @@
+'use strict'
+export { };
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively
+// (without loops or multiplication).
+
+function numberOfBunnies(bunnies: number, floppyEars: number): number {
+  
+  return
+
+}
--- week-03/day-4/power.ts
+++ week-03/day-4/power.ts
@@ -4,11 +4,13 @@ export { };
 // Given base and n that are both 1 or more, compute recursively (no loops)
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
-function baseAndN(base: number, n: number) {
-  if () {
 
-  } esle{
 
+function baseAndN(base: number, powerN: number): number {
+  if (powerN < 1) {
+    return 1
+  } else {
+    return base * (baseAndN(base, powerN - 1));
   }
 }
-console.log(baseAndN(3, 2));
+console.log(baseAndN(3, 3));
