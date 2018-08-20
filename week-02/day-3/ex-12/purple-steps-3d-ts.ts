537a5d2521e0c3cf30672d61c1c5d03bac62b353
Daniel Kasza <daniel.kasza93@gmail.com>
2018-08-20 14:02:28 +0200

    purple-steps-3d

--- week-02/day-3/ex-12/purple-steps-3d-ts.ts
+++ week-02/day-3/ex-12/purple-steps-3d-ts.ts
@@ -8,22 +8,14 @@ const ctx = canvas.getContext('2d');
 // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
 
 
-function purpleSquare(x: number, y: number, w: number, h: number) {
-
-  ctx.fillStyle = "magenta";
-  ctx.fillRect(x, y, w, h);
-  ctx.fill();
+function purpleSquare(x: number, y: number, size: number) {
+  for (let i = 0; i < 6; i++) {
+    ctx.fillStyle = "magenta";
+    ctx.fillRect(x, y, size, size);
+    ctx.fill();
+    x += size;
+    y += size;
+    size += size /2
+  }
 }
-let x = 0;
-let y = 0;
-let size = 10;
-
-for (let i = 0; i < 6; i++) {
-  x += i * size;
-  y += i * size;
-  size += i*2;
-
-  purpleSquare(x, y, size, size);
-}
-
-//nincs kesz!
+purpleSquare(0, 0, 10)
