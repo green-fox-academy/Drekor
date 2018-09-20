0320ec8804ee60f6462d1f279b0933581732ff47
Daniel Kasza <daniel.kasza93@gmail.com>
2018-09-20 23:22:07 +0200

    pi done

---
+++ week-06/day-1/pi/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// define a variable called PI with the value of 3.14
+// create a function which takes the radius of a circle as a parameter
+// and return the area of that cirle
+// area = radius * radius * PI
+
+int main()
+{
+
+	return 0;
+}
